e3dda829dfbaa75799a83162cbc60f957a89e202
team1257Simon <nsimon@ucvts.tec.nj.us>
2015-06-11 16:01:43 -0400

    Update dnd-player.js

--- dnd-player.js
+++ dnd-player.js
@@ -1,1 +1,21 @@
 
+function DnD_Player(user, plrClass)
+{
+  this.user = user;
+  this.playerClass = plrClass;
+  this.stats = plrClass.statsBase;
+  this.moves = plrClass.movesStarter;
+  this.useMove = function(move, target)
+  {
+    if(this.canUseMove(move))
+    {
+      move.Resolve(this, target);
+    }
+  }
+  this.level = 1;
+  this.upgrades = 0;
+  this.inventory = {
+    
+  }
+  //...
+}
